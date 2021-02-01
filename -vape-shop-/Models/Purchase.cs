59a0ab7438dd130446b81ac7180ae3a3c4a4310f
theacidday <stanisyan16@gmail.com>
2021-02-01 20:41:25 +0200

    Добавьте файлы проекта.

---
+++ -vape-shop-/Models/Purchase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _vape_shop_.Models
+{
+    public class Purchase
+    {
+        public int PurchasedId {get ;set;}
+        public string Person { get; set; }
+        public string 
+    }
+}
---
+++ -vape-shop-/Models/Vape.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _vape_shop_.Models
+{
+    public class Vape
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+
+        public int Mark { get; set; }
+        public int Price { get; set; }
+        public int Picture { get; set; }
+        public int Quantity { get; set; }
+    }
+}
